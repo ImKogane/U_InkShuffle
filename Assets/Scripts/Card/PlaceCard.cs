a6bfb208ccb1a103d6a4d5b8b5d1a0d0dd227fa5
Rodrigue <74072465+DumezRodrigue@users.noreply.github.com>
2023-05-23 17:33:00 +0200

    Update PlaceCard.cs

--- Assets/Scripts/Card/PlaceCard.cs
+++ Assets/Scripts/Card/PlaceCard.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlaceCard : MonoBehaviour
 {
     public GameObject card;
     public List<Transform> positionsList;
+
+
 
     private void Start()
     {
@@ -18,20 +21,58 @@ public class PlaceCard : MonoBehaviour
 
     public void SpawnCardAtRandomPosition()
     {
-        if (positionsList.Count != 0)
+        bool allTrue = CheckAllBooleansTrue();
+        if (!allTrue)
         {
-            card.GetComponent<Card>().Stats = GetComponent<SpriteLookup>().associatedScriptableObject;
+            if (positionsList.Count != 0)
+            {
+                card.GetComponent<Card>().Stats = GetComponent<SpriteLookup>().associatedScriptableObject;
 
-            int randomIndex = Random.Range(0, positionsList.Count);
-            if (positionsList[randomIndex].GetComponent<WaypointManager>().IsBusy != true ) 
-            {
-                Vector3 randomPosition = positionsList[randomIndex].position;
-                Instantiate(card, randomPosition, Quaternion.identity);
-                positionsList[randomIndex].GetComponent<WaypointManager>().IsBusy = true;
+                int randomIndex = Random.Range(0, positionsList.Count);
+                if (positionsList[randomIndex].GetComponent<WaypointManager>().IsBusy != true)
+                {
+                    Vector3 randomPosition = positionsList[randomIndex].position;
+                    Instantiate(card, randomPosition, Quaternion.identity);
+                    positionsList[randomIndex].GetComponent<WaypointManager>().IsBusy = true;
+                    ClearDeckHand();
+                }
+                else
+                {
+                    SpawnCardAtRandomPosition();
+                }
             }
-            else
+        }
+        else
+        {
+            return;
+        }
+    }
+
+    private bool CheckAllBooleansTrue()
+    {
+        foreach (Transform t in positionsList)
+        {
+            if (!t.GetComponent<WaypointManager>().IsBusy)
             {
-                SpawnCardAtRandomPosition();
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private void ClearDeckHand()
+    {
+        GameObject tempGameObject = GameObject.FindGameObjectWithTag("PlayerManager");
+        PlayerBoard tempDeck = tempGameObject.GetComponent<PlayerBoard>();
+        List<CardAttributes> tempList = tempDeck.cardsInHand;
+        foreach (CardAttributes c in tempList)
+        {
+            if (c == GetComponent<SpriteLookup>().associatedScriptableObject)
+            {
+                tempList.RemoveAt(tempList.IndexOf(c));
+                AutoCreateCarousel tempUI = FindObjectOfType<AutoCreateCarousel>();
+                tempUI.UpdateCarousel();
             }
         }
     }
