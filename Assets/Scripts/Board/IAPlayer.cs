66ba90c5dfc307be4fda76d784b4eeb261b79886
Matthias <77961470+ImKogane@users.noreply.github.com>
2023-05-24 11:10:21 +0200

    Add, IA can place card on board

--- Assets/Scripts/Board/IAPlayer.cs
+++ Assets/Scripts/Board/IAPlayer.cs
@@ -5,7 +5,22 @@ using UnityEngine;
 public class IAPlayer : MonoBehaviour
 {
 
-    [SerializeField] private PlayerBoard board;
+    [SerializeField] private PlayerBoard ownBoard;
+    [SerializeField] private PlayerBoard playerBoard;
+
+    private List<Transform> points = new List<Transform>();
+    [SerializeField] private GameObject tempCard;
+
+    private void Start()
+    {
+        GameObject[] waypoints = GameObject.FindGameObjectsWithTag("IACardPlaces");
+        Debug.Log(waypoints.Length);
+
+        foreach (GameObject w in waypoints)
+        {
+            points.Add(w.gameObject.transform);
+        }
+    }
 
     public void Attack()
     {
@@ -14,6 +29,52 @@ public class IAPlayer : MonoBehaviour
 
     public void PutCard()
     {
+        int randCardIndex = Random.Range(0, ownBoard.cardsInHand.Count);
+
+        bool allTrue = CheckAllBooleansTrue();
+        if (!allTrue)
+        {
+            if (points.Count != 0)
+            {
+                tempCard.GetComponent<Card>().Stats = ownBoard.cardsInHand[randCardIndex];
+                //GameObject card = ownBoard.cardsInHand[randCardIndex].
+
+                int randomPosIndex = Random.Range(0, points.Count);
+                if (points[randomPosIndex].GetComponent<WaypointManager>().IsBusy != true)
+                {
+                    Vector3 randomPosition = points[randomPosIndex].position;
+                    Quaternion randomRot = points[randomPosIndex].rotation;
+                    GameObject placedCard =  Instantiate(tempCard, randomPosition, randomRot);
+                    points[randomPosIndex].GetComponent<WaypointManager>().IsBusy = true;
+                    ownBoard.cardsInHand.RemoveAt(randCardIndex);
+
+                    if (placedCard.GetComponent<Card>() != null) placedCard.GetComponent<Card>().actualCardSide = Card.cardSide.AICARD;
+
+                }
+                else
+                {
+                    PutCard();
+                }
+            }
+        }
+        else
+        {
+            return;
+        }
 
     }
+
+    private bool CheckAllBooleansTrue()
+    {
+        foreach (Transform t in points)
+        {
+            if (!t.GetComponent<WaypointManager>().IsBusy)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
 }
