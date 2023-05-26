4907d81345d172b012779a14c5f58fb1b382c491
Matthias <77961470+ImKogane@users.noreply.github.com>
2023-05-26 10:06:23 +0200

    Add, Pause menu

---
+++ Assets/UI_Pause.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UI_Pause : MonoBehaviour
+{
+    private bool isPause = false;
+    [SerializeField] private GameObject pauseCanvas;
+
+    public void SetPause()
+    {
+        if(isPause && pauseCanvas != null)
+        {
+            Time.timeScale = 1;
+            pauseCanvas.SetActive(false);
+            isPause = false;
+        }
+        else
+        {
+            Time.timeScale = 0;
+            pauseCanvas.SetActive(true);
+            isPause = true;
+        }
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("MainMenuScene");
+    }
+}
